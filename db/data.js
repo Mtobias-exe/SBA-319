9d4d3d45542c9cb2465ad274a15317707e802b5e
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-16 00:40:02 -0500

    added starter pastry

---
+++ db/data.js
@@ -0,0 +1,21 @@
+const starterPastry = [
+    {  name: "Croissant", 
+    description: "A buttery, flaky pastry with a golden, crisp exterior and a soft, airy interior. Perfectly baked for a delicate crunch in every bite, this timeless French treat is ideal for breakfast or a mid-day indulgence.",
+        price: 6,
+        imgUrl: "https://th.bing.com/th/id/R.7336a0dcdcf2f4151a391353f3925ff4?rik=Uu5TRHFMKWImfQ&pid=ImgRaw&r=0"
+    },
+
+    {  name: "Baklava", 
+        description: "A rich, sweet pastry made from layers of delicate filo dough, generously filled with a blend of finely chopped nuts and sweetened with honey or syrup.",
+            price: 6.5,
+            imgUrl: "https://tse1.mm.bing.net/th?id=OIP.6r1V6UOauaaUj4O7eL4gSQHaE8&rs=1&pid=ImgDetMain"
+    },
+
+    {  name: "Biko", 
+        description: "A traditional Filipino sweet rice cake made with sticky rice, coconut milk, and brown sugar, creating a rich, chewy texture that melts in your mouth.",
+            price: 5,
+            imgUrl: "https://getrecipe.org/wp-content/uploads/2021/04/Bico-11-1024x576.png"
+    },
+];
+
+module.exports = starterPastry
