c9b2195efbfd0a62fc20ad2bc05fcedb2d87893a
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-16 22:35:12 -0500

    finised reviews route

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -13,4 +13,35 @@ router.get('/', async(req, res)=>{
     }
 });
 
+//add new review
+router.post('/', async(req, res) => {
+    try {
+      const createdReview = await Reviews.create(req.body) 
+      console.log(req.body)
+      res.json(createdReview)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+//edit review
+router.put('/:id', async(req, res)=>{
+  try {
+    const updatedReview = await Reviews.findByIdAndUpdate(req.params.id, req.body)
+    res.json(updatedReview)  
+  } catch (error) {
+      res.status(500).json({error: error.message})
+  }
+})
+
+//delete review
+router.delete('/:id', async (req, res)=>{
+  try {
+  const deletedReview = await Reviews.findByIdAndDelete(req.params.id);
+  res.json(deletedReview);
+  } catch (error) {
+  res.status(500).json({error: error.message})
+  }
+})
+
 module.exports = router
